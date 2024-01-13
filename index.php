a96273131819a98be05a0882626e766744e3d399
quangan186 <quangan186@gmail.com>
2024-01-13 13:31:11 +0700

    setup rds

--- index.php
+++ index.php
@@ -90,9 +90,9 @@
             <div class="row rmit-product-section">
               <?php
               $link = mysqli_connect(
-                  "localhost",
-                  "db_admin",
-                  "rmit_password",
+                  "team7-rds-db.cpyusuw42c0c.us-east-1.rds.amazonaws.com",
+                  "admin",
+                  "s3877482",
                   "rmit_store_db"
               );
               if ($link) {
