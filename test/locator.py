0b81946dd998367964dabc2813d5c5e783d7092b
quangan186 <quangan186@gmail.com>
2024-01-19 20:09:48 +0700

    setup test

--- test/locator.py
+++ test/locator.py
@@ -2,4 +2,4 @@ from faker import Faker
 
 fake = Faker(locale='en_CA')
 rmit_store_dev_url = 'http://35.153.136.254:8080/'
-rmit_store_prod_url = 'http://35.153.136.254:8080/'
+rmit_store_prod_url = 'ababa054f044441e79b928e73188013a-141815326.us-east-1.elb.amazonaws.com'
