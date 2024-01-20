fc39b34e7b2f1ba52edb6329d519bd387a6789e5
quangan186 <quangan186@gmail.com>
2024-01-20 10:14:11 +0700

    update dev IP

--- test/locator.py
+++ test/locator.py
@@ -1,5 +1,5 @@
 from faker import Faker
 
 fake = Faker(locale='en_CA')
-rmit_store_dev_url = 'http://3.88.148.251/'
+rmit_store_dev_url = 'http://54.92.227.80/'
 rmit_store_prod_url = 'http://ababa054f044441e79b928e73188013a-141815326.us-east-1.elb.amazonaws.com/'
