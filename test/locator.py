5e5892849168949093025dc4e8be46fe5c0b63b0
quangan186 <quangan186@gmail.com>
2024-01-20 10:24:20 +0700

    update lb url

--- test/locator.py
+++ test/locator.py
@@ -2,4 +2,4 @@ from faker import Faker
 
 fake = Faker(locale='en_CA')
 rmit_store_dev_url = 'http://54.92.227.80/'
-rmit_store_prod_url = 'http://ababa054f044441e79b928e73188013a-141815326.us-east-1.elb.amazonaws.com/'
+rmit_store_prod_url = 'http://acb3bb145d9ef416a83cd4a142c32a2a-1239393072.us-east-1.elb.amazonaws.com/'
